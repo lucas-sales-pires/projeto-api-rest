984aecccc4114a19e43163671054e5ce8d8a7188
lucas-sales-pires <lucas2013sales@hotmail.com>
2024-06-07 20:26:29 -0300

    .

--- dist/index.js
+++ dist/index.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = require("dotenv");
-const banco_js_1 = require("./banco.js");
-const chat_rota_js_1 = require("./rotas/chat_rota.js");
+const banco_js_1 = require("./src/models/banco.js");
+const chat_rota_js_1 = require("./src/rotas/chat_rota.js");
 const express_1 = __importDefault(require("express"));
 (0, dotenv_1.config)();
 const porta = process.env.PORTA;
