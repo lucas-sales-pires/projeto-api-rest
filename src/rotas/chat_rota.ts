0206b298fa26eb9079dfb0ab746a81e1eddbbdd3
lucas-sales-pires <lucas2013sales@hotmail.com>
2024-06-08 15:12:48 -0300

    Update chat_rota.ts

--- src/rotas/chat_rota.ts
+++ src/rotas/chat_rota.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { buscarMensagemParaVoce, buscarMensagensAnteriores, enviarMensagemParaUsuario,buscarMensagemParaUsuario,buscarMensagemParaAdm,usuarioEnviarMensagemParaAdm } from '../controller/mensagemController';
 
-export const routerMensagem = express.Router();
+export const routerMensagem = express();
 
 routerMensagem.get('/anteriores/:sala', buscarMensagensAnteriores);
 routerMensagem.get('/mensagens/:nome', buscarMensagemParaVoce);
